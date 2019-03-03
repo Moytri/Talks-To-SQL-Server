e42f64e6fdd7687734e52168692443a74852941e
moytri <moytritopa@gmail.com>
2019-03-02 16:27:43 -0800

    indentetion

--- Assign04/Assign04/Program.cs
+++ Assign04/Assign04/Program.cs
@@ -11,6 +11,7 @@ namespace Assign04
         static void Main(string[] args)
         {
             List<string> provinces = CustomerRepository.GetDistinctProvinces();
+
             CustomerPrinter.PrintProvinces(provinces);
         }
     }
